0782c384342fa0e62c84c6110d158867b8f67ae0
YardenSamorai <Yardensamorai6@gmail.com>
2022-10-10 19:18:01 +0300

    fix Responsive bugs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,14 @@ import './Footer.css'
 
 const Footer = () => {
 
+  const handleOnClickFacebook = () => {
+    window.open("https://www.facebook.com/Chayxana-%D7%A6%D7%99%D7%99%D7%97%D7%A0%D7%94-111346931694188", '_blank');
+  }
+
+  const handleOnClickWhatappMsg = () => {
+    const text= "היי%20ברצוני%20לקבל%20פרטים%20נוספים%20על%20מסעדת%20צ'ייחנה"
+    window.open(`https://wa.me/+972544818272?text=${text}`,"_blank");
+  }
   const texts = {
     location: {
       title: "מיקום",
@@ -42,16 +50,18 @@ const Footer = () => {
             <div className="col">
               <h4>{texts.contactUs}</h4>
               <ul className="list-unstyled">
-                <li>{texts.hours.sunToThur}</li>
-                <li>{texts.hours.friday}</li>
-                <li>{texts.hours.saturday}</li>
+              <li>03-7739919</li>
+                <li>
+                  <img src={require("../../assets/Icons/facebook.png")} onClick={handleOnClickFacebook} />
+                  <img src={require("../../assets/Icons/whatsapp.png")} onClick={handleOnClickWhatappMsg}/>
+                </li>
               </ul>
             </div>
           </div>
           <div className="box-2 col embd-map">
-              {/* <MapBox /> */}
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d597.5310203190166!2d34.88519564855042!3d32.09234248328812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d366cbd9ffa4d%3A0x999553a1b6554d41!2sBar%20Kochva%20St%2032%2C%20Petah%20Tikva!5e0!3m2!1sen!2sil!4v1665327261053!5m2!1sen!2sil" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+            <h4>דרכי הגעה</h4>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d597.5312340653215!2d34.88553272723686!3d32.09230980077325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d366cbd9ffa4d%3A0x999553a1b6554d41!2sBar%20Kochva%20St%2032%2C%20Petah%20Tikva!5e0!3m2!1sen!2sil!4v1665415193374!5m2!1sen!2sil" max-width="400" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
           <div className="box-3 col footer-logo">
             <Logo />
           </div>
